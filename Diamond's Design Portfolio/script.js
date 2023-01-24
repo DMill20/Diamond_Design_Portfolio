7acda62791aaad64e7fc03e81ae685dbd06a4193
Diamond M <89270895+DMill20@users.noreply.github.com>
2023-01-24 14:57:51 -0600

    Delete Diamond's Design Portfolio directory

--- Diamond's Design Portfolio/script.js
+++
@@ -1,6 +0,0 @@
-var tglbtn = document.getElementById("nav-toggle");
-var navlst = document.getElementById("nav-list");
-
-tglbtn.addEventListener("click", () => {
-  navlst.classList.toggle("active");
-});
